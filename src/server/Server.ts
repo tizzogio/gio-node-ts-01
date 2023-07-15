88da4343f9a052e2b34ae283bbeec6abf9eddbd6
tizzoGio <giovani.tizzo@gmail.com>
2023-07-15 16:45:36 -0300

    First commit

---
+++ src/server/Server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const server = express(); //instancia basica do servidor
+
+server.get('/', (req, res) => {
+  return res.send('Olá dev');
+} );
+
+
+export { server };
